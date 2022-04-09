a4dae27f96b7f954d45617bce2649554bff3f388
Wooseok Jang <mmyu2090@gmail.com>
2022-04-09 19:55:11 +0900

    root api added

--- app/api.py
+++ app/api.py
@@ -1,6 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from app.db import DB
 
 
+User = []
+
 app = FastAPI()
 mydb = DB()
+
+origins = ["http://localhost", "http://localhost:3000",
+           "http://localhost:8000", "http://localhost:8080",
+           "https://localhost", "https://localhost:3000",
+           "https://localhost:8000", "https://localhost:8080",
+           "localhost", "localhost:3000",
+           "localhost:8000", "localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['GET', 'POST', "DELETE", 'OPTIONS'],
+    allow_headers=['*'],
+)
+
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    item = {"message": "WORKING !!!"}
+    return JSONResponse(status_code=status.HTTP_200_OK, content=item)
--- app/db.py
+++ app/db.py
@@ -1,12 +1,17 @@
+from types import NoneType
 import pymysql
 from app.models.user import User
 
 
 class DB(object):
 
-    conn = None
-    cur = None
-    sql = ""
+    _conn = None
+    _cur = None
+    _sql = ""
+    _err = False
+
+    def DBOK(self):
+        return DB.err
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(cls, "_instance"):
@@ -19,58 +24,85 @@ class DB(object):
         return cls._instance
 
     def __init__(self):
-        DB.conn = pymysql.connect(host='localhost', user='FarmFactory',
-                                  password='Nekarakube!1', db='FFDB', charset='utf8mb4')
-        DB.cur = DB.conn.cursor()
-        DB.sql = "CREATE TABLE IF NOT EXISTS User(id int NOT NULL AUTO_INCREMENT, Name char(30), Team char(30), Nickname char(30), Emailaddr char(30), Password char(30), ProfilePic char(125), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Team(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Project(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Object(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
         print("\033[32m" + "DB  " + "\033[0m", end=':     ')
-        print("DB Table initiation complete.")
+        print("DB initiation started")
+        DB._connectDB(self)
+        if not DB._err:
+            DB._createTable(self)
+            print("\033[32m" + "DB  " + "\033[0m", end=':     ')
+            print("DB connection confirmed")
 
     def __del__(self):
-        DB.conn.close()
+        if DB._conn != None:
+            DB._conn.close()
+
+    def _createTable(self):
+        if not DB._err:
+            DB._sql = "CREATE TABLE IF NOT EXISTS User(id int NOT NULL AUTO_INCREMENT, Name char(30), Team char(30), Nickname char(30), Emailaddr char(30), Password char(30), ProfilePic char(125), PRIMARY KEY (id), UNIQUE(id))"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "CREATE TABLE IF NOT EXISTS Team(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "CREATE TABLE IF NOT EXISTS Project(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "CREATE TABLE IF NOT EXISTS Object(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._conn.close()
+
+    def _dropTable(self):
+        if not DB._err:
+            DB._sql = "DROP TABLE User"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "DROP TABLE Team"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "DROP TABLE Project"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._sql = "DROP TABLE Object"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            DB._conn.close()
+
+    def _connectDB(self):
+        try:
+            DB._conn = pymysql.connect(
+                host='localhost', user='FarmFactory', password='Nekarakube!1', db='FFDB', charset='utf8mb4')
+        except pymysql.err.OperationalError:
+            DB.err = True
+            print("\033[31m" + "DB  " + "\033[0m", end=':     ')
+            print("DB CONNECTION FAILED !!!")
+        else:
+            DB.err = False
+            DB._cur = DB._conn.cursor()
 
     def WarnTestDelAllTableData(self):
-        DB.sql = "DROP TABLE User"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "DROP TABLE Team"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "DROP TABLE Project"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "DROP TABLE Object"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS User(id int NOT NULL AUTO_INCREMENT, Name char(30), Team char(30), Nickname char(30), Emailaddr char(30), Password char(30), ProfilePic char(125), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Team(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Project(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
-        DB.sql = "CREATE TABLE IF NOT EXISTS Object(id int NOT NULL AUTO_INCREMENT, Name char(30), Owner int, Data char(255), ProfilePic char(255), PRIMARY KEY (id), UNIQUE(id))"
-        DB.cur.execute(DB.sql)
-        DB.conn.commit()
+        DB._connectDB(self)
+        DB._dropTable(self)
+        DB._connectDB(self)
+        DB._createTable(self)
         print("\033[33m" + "DB  " + "\033[0m", end=':     ')
         print("All tables DROPED and CREATED")
 
-
-"""
     def addUser(self, user: User):
-        DB.sql = "INSERT INTO User(Name, Nickname, Emailaddr, Password) VALUES("
+        if not DB.err:
+            DB._sql = "INSERT INTO User(Name, Nickname, Emailaddr, Password) VALUES('" + user.Name + \
+                "', " + user.Nickname + "', " + user.Emailaddr + "', " + user.Password + "')"
+            DB._cur.execute(DB._sql)
+            DB._conn.commit()
+            return True
+        else:
+            return False
 
-"""
+    def getUser(self, user: User):
+        if not DB.err:
+            DB._sql = "SELECT * FROM userTable WHERE Emailaddr='" + user.Emailaddr + "'"
+            DB._cur.execute(DB._sql)
+            row = DB._cur.fetchone()
+            return True, row
+        else:
+            return False, None
